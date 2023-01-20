8d89935f6e1ee3ca05ad6f19d49f093c48f48750
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-20 23:12:49 +0100

    init

--- src/main.c
+++ src/main.c
@@ -1,6 +1,33 @@
 #include "ft_ping.h"
 
+int init_v4(void);
+int init_v6(void);
+int proc_v4(void);
+int proc_v6(void);
+int send_v4(void);
+int send_v6(void);
+
+t_proto proto_v4 = {
+	init_v4, 
+	proc_v4, 
+	send_v4, 
+	IPPROTO_ICMP
+};
+
+t_proto proto_v6 = {
+	init_v6, 
+	proc_v6, 
+	send_v6, 
+	IPPROTO_ICMPV6
+};
+
+int calculate_checksum(int type, int checksum, int id, int seq)
+{
+	return 0xFFFF - (type + checksum + id + seq);
+}
+
 int main(int argc, char **argv)
 {
+	
 	return (0);
 }
