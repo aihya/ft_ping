e99cd1b6a6138b4afdbeb3fa9dd18c1a84a33b7f
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-21 18:58:07 +0100

    -

--- src/main.c
+++ src/main.c
@@ -95,8 +95,10 @@ void proc_v4(int sockfd)
 	tvrecv = *(struct timeval *)(icmp->icmp_data);
 
 	gettimeofday(&tvcurr, NULL);
-	printf("%ld\n", ((tvcurr.tv_usec) / 100 - (tvrecv.tv_usec) / 100));
-	time = (tvcurr.tv_sec * 1000 + (tvcurr.tv_usec) / 1000) - (tvrecv.tv_sec * 1000 + (tvrecv.tv_usec) / 1000);
+	time  = (tvcurr.tv_sec  - tvrecv.tv_sec)  * 1000;
+	time += (tvcurr.tv_usec - tvrecv.tv_usec) / 1000;
+	time += (tvcurr.tv_usec - tvrecv.tv_usec) % 100
+	printf("%lf\n", time);
 	printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2lf ms\n", 64, buf, buf, icmp->icmp_seq, ip->ip_ttl, time);
 }
 
