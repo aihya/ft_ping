b69f84918d0368a56990fee55c9e6ae2a0b99307
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-21 19:10:33 +0100

    -

--- src/main.c
+++ src/main.c
@@ -98,7 +98,7 @@ void proc_v4(int sockfd)
 	time  = (tvcurr.tv_sec  - tvrecv.tv_sec)  * 1000;
 	time += (tvcurr.tv_usec - tvrecv.tv_usec) / 1000.0;
 	printf("%ld %ld\n", (tvcurr.tv_usec - tvrecv.tv_usec), (tvcurr.tv_usec - tvrecv.tv_usec) % 1000);
-	printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%lf ms\n", 64, buf, buf, icmp->icmp_seq, ip->ip_ttl, time);
+	printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%.2lf ms\n", 64, buf, buf, icmp->icmp_seq, ip->ip_ttl, time);
 }
 
 int calculate_checksum(int id, int seq)
