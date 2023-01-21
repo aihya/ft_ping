abf4d037e9f72e2064b8f51924280c9346de00d5
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-21 18:32:22 +0100

    -

--- src/main.c
+++ src/main.c
@@ -76,7 +76,7 @@ void proc_v4(int sockfd)
 	if (ip->ip_p == IPPROTO_ICMP)
 		printf("IPPROTO_ICMP\n");
 
-	icmp = (struct icmp *)(iov.iov_base + (ip->ip_hl << 2));
+	icmp = (struct icmp *)(iov.iov_base + (ip->ip_hl));
 	if (icmp->icmp_type == ICMP_ECHOREPLY)
 		printf("ICMP_ECHOREPLY %d\n", icmp->icmp_id);
 
