52dcfc9bf90d6ced393648aa041189070b11d541
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-01-21 13:28:27 +0100

    test

--- src/main.c
+++ src/main.c
@@ -1,41 +1,64 @@
 #include "ft_ping.h"
 
-int init_v4(void);
-int init_v6(void);
-int proc_v4(void);
-int proc_v6(void);
-int send_v4(void);
-int send_v6(void);
+// int init_v4(void);
+// int init_v6(void);
+// int proc_v4(void);
+// int proc_v6(void);
+// int send_v4(void);
+// int send_v6(void);
 
-t_proto proto_v4 = {
-	init_v4, 
-	proc_v4, 
-	send_v4, 
-	IPPROTO_ICMP
-};
+// t_proto proto_v4 = {
+// 	init_v4, 
+// 	proc_v4, 
+// 	send_v4, 
+// 	IPPROTO_ICMP
+// };
 
-t_proto proto_v6 = {
-	init_v6, 
-	proc_v6, 
-	send_v6, 
-	IPPROTO_ICMPV6
-};
+// t_proto proto_v6 = {
+// 	init_v6, 
+// 	proc_v6, 
+// 	send_v6, 
+// 	IPPROTO_ICMPV6
+// };
 
 int calculate_checksum(int type, int checksum, int id, int seq)
 {
 	return 0xFFFF - (type + checksum + id + seq);
 }
 
-int main(int argc, char **argv)
+int resolve_destination(char *target)
 {
-	struct addrinfo ai;
 	struct addrinfo hints;
-	struct addrinfo **res;
+	struct addrinfo *result;
+	struct addrinfo *ptr;
+	int ret;
 
 	hints.ai_family = AF_INET;
 	hints.ai_protocol = IPPROTO_TCP;
-	int ret = getaddrinfo("google.com", "http", &hints, res);
+	ret = getaddrinfo(target, "http", &hints, &result);
+	if (ret)
+		return (1);
+	return (0);
+}
 
-	printf("%d\n", ret);
+int gai_error(char *exe, char *dest, int error)
+{
+	fprintf(stderr, "%s: %s: %s", exe, dest, gai_strerror(error));
+	return (1);
+}
+
+int main(int argc, char **argv)
+{
+	int error;
+
+	if (argc == 1)
+	{
+		fprintf(stderr, "%s: usage error: Destination address required", argv[0]);
+		return (1);
+	}
+
+	error = resolve_destination(argv[1]);
+	if (error)
+		return (gai_error(argv[0], argv[1], error));
 	return (0);
 }
