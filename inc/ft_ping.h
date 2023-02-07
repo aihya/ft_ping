d07b5fff314e014898057fa6753dffbd608423bc
root <abdechahid.ihya@hotmail.com>
2023-02-07 10:47:52 -0500

    -

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -10,7 +10,6 @@
 # include <signal.h>
 # include <unistd.h>
 # include <string.h>
-# include <strings.h> // TODO: remove this line and bzero function in main
 # include <netinet/ip_icmp.h>
 # include <netinet/ip.h>
 # include <sys/time.h>
@@ -19,6 +18,27 @@
 
 #define IPV4_HDRLEN (sizeof(struct ip))
 #define ICMP_HDRLEN (sizeof(struct icmp) - IPV4_HDRLEN)
+
+#define ICMP_T0_C0 	    "Destination network unreachable"
+#define ICMP_T0_C1   	"Destination host unreachable"
+#define ICMP_T0_C2 	    "Destination protocol unreachable"
+#define ICMP_T0_C3   	"Destination port unreachable"
+#define ICMP_T0_C4 	    "Fragmentation required, and DF flag set"
+#define ICMP_T0_C5 	    "Source route failed"
+#define ICMP_T0_C6   	"Destination network unknown"
+#define ICMP_T0_C7 	    "Destination host unknown"
+#define ICMP_T0_C8  	"Source host isolated"
+#define ICMP_T0_C9 	    "Network administratively prohibited"
+#define ICMP_T0_C10 	"Host administratively prohibited"
+#define ICMP_T0_C11 	"Network unreachable for ToS"
+#define ICMP_T0_C12 	"Host unreachable for ToS"
+#define ICMP_T0_C13 	"Communication administratively prohibited"
+#define ICMP_T0_C14 	"Host Precedence Violation"
+#define ICMP_T0_C15 	"Precedence cutoff in effect" 
+
+#define ICMP_T11_C0 	"Time to live exceeded"
+#define ICMP_T11_C1 	"Fragment reassembly time exceeded"
+
 
 // Reference of functions used
 enum e_function
@@ -61,6 +81,8 @@ typedef struct s_data
     int     sock_fd;
     t_dest  dest;
 
+    char *target;
+
     // Packet sent or not
     int sent;
 
@@ -71,7 +93,13 @@ typedef struct s_data
     uint16_t checksum;
 
     // Hostname from dns lookup
-    char hostname[4029];
+    char hostname[4096];
+
+    // Presentable format
+    char presentable[4096];
+
+    // ICMP packet error message
+    char packet_error[256];
 
     // Error management.
     enum e_error_type   type;
--- src/main_2.c
+++ src/main_2.c
@@ -27,6 +27,19 @@ void *ft_memset(void *s, int c, size_t n)
 }
 
 
+int		ft_strcmp(const char *s1, const char *s2)
+{
+	const char *s1_cpy;
+	const char *s2_cpy;
+
+	s1_cpy = s1;
+	s2_cpy = s2;
+	while ((*s1_cpy || *s2_cpy) && (*s1_cpy++ == *s2_cpy++))
+		;
+	return ((unsigned char)*(s1_cpy - 1) - (unsigned char)*(s2_cpy - 1));
+}
+
+
 void *ft_memcpy(void *dst, const void *src, size_t n)
 {
     unsigned char *udst;
@@ -42,6 +55,12 @@ void *ft_memcpy(void *dst, const void *src, size_t n)
         i++;
     }
     return dst;
+}
+
+void set_error_codes(enum e_function function, enum e_error_type type, enum e_error error)
+{
+    g_data.function = function;
+    g_data.error = error;
 }
 
 
@@ -87,16 +106,16 @@ int send_icmp_packet()
 
     // Construct ICMP packet
     icmp = (struct icmp *)packet;
-    icmp->icmp_type  = ICMP_ECHO;
     icmp->icmp_code  = 0;
+    icmp->icmp_type  = ICMP_ECHO;
     icmp->icmp_seq   = ++sequence;
     icmp->icmp_id    = getpid();
-    icmp->icmp_cksum = 0;
 
     // Calculate the ICMP checksum
     icmp->icmp_cksum = calculate_checksum((uint16_t *)packet, sizeof(packet));
+    g_data.checksum = icmp->icmp_cksum;
 
-    // Store the current time befire the packet is sent
+    // Store the current time before the packet is sent
     gettimeofday(&(g_data.send_time), 0);
 
     // Send the packet
@@ -105,21 +124,108 @@ int send_icmp_packet()
     ret = sendto(g_data.sock_fd, packet, sizeof(packet), 0, destaddr, addrlen);
     if (ret < 0)
     {
+        perror("sendto");
         set_error_codes(SENDTO, FUNCTION, 0);
         return (-1);
     }
+    g_data.sent = 1;
     return (1);
 }
 
 
-void receive_icmp_packet()
+void set_destination_unreachable(int code)
+{
+    switch (code)
+    {
+        case 0:
+            g_data.packet_error = ICMP_T0_C0;
+            break ;
+        case 1:
+            g_data.packet_error = ICMP_T0_C1;
+            break ;
+        case 2:
+            g_data.packet_error = ICMP_T0_C2;
+            break ;
+        case 3:
+            g_data.packet_error = ICMP_T0_C3;
+            break ;
+        case 4:
+            g_data.packet_error = ICMP_T0_C4;
+            break ;
+        case 5:
+            g_data.packet_error = ICMP_T0_C5;
+            break ;
+        case 6:
+            g_data.packet_error = ICMP_T0_C6;
+            break ;
+        case 7:
+            g_data.packet_error = ICMP_T0_C7;
+            break ;
+        case 8:
+            g_data.packet_error = ICMP_T0_C8;
+            break ;
+        case 9:
+            g_data.packet_error = ICMP_T0_C9;
+            break ;
+        case 10:
+            g_data.packet_error = ICMP_T0_C10;
+            break ;
+        case 11:
+            g_data.packet_error = ICMP_T0_C11;
+            break ;
+        case 12:
+            g_data.packet_error = ICMP_T0_C12;
+            break ;
+        case 13:
+            g_data.packet_error = ICMP_T0_C13;
+            break ;
+        case 14:
+            g_data.packet_error = ICMP_T0_C14;
+            break ;
+        case 15:
+            g_data.packet_error = ICMP_T0_C15;
+            break ;
+    }
+}
+
+
+void set_time_exceeded(int code)
+{
+    
+}
+
+
+void set_packet_error_message(int type, int code)
+{
+    switch (type)
+    {
+        case 0:
+            set_destination_unreachable(code);
+            break;
+        case 11:
+            set_time_exceeded(code);
+            break;
+        default:
+            // TODO: Must manage the -v option here.
+            break;
+    }
+}
+
+
+int receive_icmp_packet()
 {
     char packet[IP_MAXPACKET];
     struct ip *ip;
     struct icmp *icmp;
-    struct msghdr msg;
     struct iovec iov;
+    struct msghdr msg;
+    struct timeval current_time;
+    int icmp_len;
     int ret;
+    uint16_t checksum;
+    double time;
+
+    ft_memset(packet, 0x00, sizeof(packet));
 
     // Construct iov structure
     iov.iov_base = packet;
@@ -135,31 +241,55 @@ void receive_icmp_packet()
     msg.msg_iovlen = 1;
 
     // Start receiving a response
-    ret = recvmsg(g_data.sock_fd, &msg, 0);
-    if (ret < 0)
+    while ((ret = recvmsg(g_data.sock_fd, &msg, 0)) > 0)
     {
-        set_error_codes(RECVMSG, FUNCTION, 0);
-        return (-1);
-    }
-
-    ip = (struct ip *)packet;
-    if (ip->ip_p != IPPROTO_ICMP || ip->ip_v != AF_INET)
-    {
-        exit(0);
-    }
-    icmp = (struct icmp *)(packet + (ip->ip_hl << 2));
     
+        if (ret < 0)
+        {
+            set_error_codes(RECVMSG, FUNCTION, 0);
+            return (-1);
+        }
+
+        ip = (struct ip *)packet;
+        if (ip->ip_p == IPPROTO_ICMP && ip->ip_v == IPVERSION)
+        {
+            icmp = (struct icmp *)(packet + (ip->ip_hl << 2));
+            checksum = calculate_checksum((uint16_t *)icmp, sizeof(packet) - IPV4_HDRLEN);
+            if (icmp->icmp_type == ICMP_ECHOREPLY && icmp->icmp_code == 0 && icmp->icmp_id == getpid() && checksum == 0)
+                break;
+        }
+    }
+
+    gettimeofday(&current_time, 0);
+    time  = (current_time.tv_sec  - g_data.send_time.tv_sec)  * 1000.;
+    time += (current_time.tv_usec - g_data.send_time.tv_usec) / 1000.;
+
+    if (!ft_strcmp(g_data.target, g_data.presentable))
+    {
+        printf("%d bytes from %s: icmp_seq=%d ttl=%d time=%.1lf ms\n", 
+            ret - IPV4_HDRLEN,
+            g_data.presentable, 
+            icmp->icmp_seq, 
+            ip->ip_ttl, 
+            time
+        );
+    }
+    else
+    {
+        printf("%d bytes from %s (%s): icmp_seq=%d ttl=%d time=%.1lf ms\n", 
+            ret - IPV4_HDRLEN,
+            g_data.hostname, 
+            g_data.presentable, 
+            icmp->icmp_seq, 
+            ip->ip_ttl, 
+            time
+        );
+    }
+    g_data.sent = 0;
+    return (0);
 }
 
-
-void set_error_codes(enum e_function function, enum e_error_type type, enum e_error error)
-{
-    g_data.function = function;
-    g_data.error = error;
-}
-
-
-int get_hostname()
+int set_hostname()
 {
     int ret;
 
@@ -167,10 +297,19 @@ int get_hostname()
                     g_data.hostname, sizeof(g_data.hostname), NULL, 0, 0)) < 0)
     {
         set_error_codes(GETNAMEINFO, FUNCTION, ret);
-        return (1);
+        return (-1);
     }
 
-    return (-1);
+    return (1);
+}
+
+
+void set_presentable_format()
+{
+    struct sockaddr_in *sa_in;
+
+    sa_in = (struct sockaddr_in *)(g_data.dest.sa);
+    inet_ntop(AF_INET, &(sa_in->sin_addr), g_data.presentable, sizeof(g_data.presentable));
 }
 
 
@@ -187,17 +326,21 @@ struct addrinfo *resolve_target(char *target)
 
     if ((ret = getaddrinfo(target, NULL, &hints, &res)))
     {
+        printf("getaddrinfo: %s\n", gai_strerror(ret));
         set_error_codes(GETADDRINFO, FUNCTION, ret);
         return NULL;
     }
 
-    for (p = res; p; p = p->ai_next)
+    for (p = res; p != NULL; p = p->ai_next)
     {
-        if (p->ai_family == AF_INET &&
+        if (p->ai_family   == AF_INET &&
             p->ai_protocol == IPPROTO_ICMP &&
-            p->ai_socktype == SOCK_RAW)
+            p->ai_socktype == SOCK_RAW &&
+            p->ai_flags    == 0)
             break;
     }
+    if (p == NULL)
+        return (NULL);
     result = calloc(1, sizeof(struct addrinfo));
     ft_memcpy(result, p, sizeof(struct addrinfo));
     return result;
@@ -232,10 +375,20 @@ void loop()
     signal_handler(SIGALRM);
     while (true)
     {
-       if (g_data.sent)
-           receive_icmp_packet();
+        if (g_data.sent)
+        {
+            receive_icmp_packet();
+        }
         usleep(10);
     }
+}
+
+
+int setup_socket()
+{
+    g_data.sock_fd = socket(AF_INET, SOCK_RAW, IPPROTO_ICMP);
+
+    return g_data.sock_fd;
 }
 
 
@@ -250,13 +403,20 @@ int main(int argc, char **argv)
 
     g_data.dest.sa = g_data.dest.ai->ai_addr;
 
-    if (!get_hostname())
+    if (set_hostname() < 0)
     {
         // TODO: Print error message here.
         return (1);
     }
 
-    printf("[Target]: %s\n", g_data.hostname);
+    set_presentable_format();
+    if (setup_socket() < 0)
+    {
+        perror("socket");
+        exit(1);
+    }
+
+    g_data.target = argv[1];
     loop();
     return (0);
 }
