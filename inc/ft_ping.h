84ab0c3f05ec7edacdbe3cfeb54ee7904b255d54
Abdechahid Ihya <abdechahid.ihya@hotmail.com>
2023-02-08 13:34:40 +0100

    -

--- inc/ft_ping.h
+++ inc/ft_ping.h
@@ -105,7 +105,7 @@ void	print_verbose(void);
 // utils.c
 int		ft_strlen(const char *str);
 void	*ft_memset(void *s, int c, size_t n);
-int		ft_strcmp(const char *s1, const char *s2);
+int		ft_strncmp(const char *s1, const char *s2);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
 
 // info.c
